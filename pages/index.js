f3bd2001984a667c8c83e0d60d0a61f2e689519c
Dongwan KANG <emasion@geninetworks.com>
2022-01-14 10:13:32 +0900

    change package.json

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import styles from '../styles.module.css'
 
-export default function Home() {
+export default function Intro() {
     return (
         <div className={styles.hello}>
             <p>Hello World</p>
